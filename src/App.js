c8c21dfc0171f2ef97a2ffebc7f1eef877e43b0f
callmeisma <ismael.fdez87@gmail.com>
2023-05-11 17:38:07 -0400

    adding pages and routes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import "./App.css";
 import Nav from "./components/Nav";
 import Home from "./components/Home";
 import About from "./components/About";
+import Services from "./components/Services";
+import Courts from "./components/Courts";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -12,6 +15,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/courts" element={<Courts />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </div>
   );
---
+++ src/components/Courts.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Courts() {
+  return (
+    <div className="">
+      <h1>Courts</h1>
+    </div>
+  );
+}
+
+export default Courts;
